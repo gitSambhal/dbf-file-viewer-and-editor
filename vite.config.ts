911c95a17b33727ad1cc6793c997600679d82452
Suhail Akhtar <sambhal@outlook.com>
2026-01-23 12:06:29 +0530

    feat: fix offline support, app can now run in offline mode without internet

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
@@ -9,7 +10,28 @@ export default defineConfig(({ mode }) => {
         port: 3000,
         host: '0.0.0.0',
       },
-      plugins: [react()],
+      plugins: [
+        react(),
+        VitePWA({
+          manifest: {
+            name: 'DBF Nexus',
+            short_name: 'DBFNexus',
+            theme_color: '#4f46e5',
+            icons: [
+              {
+                src: 'pwa-192x192.png',
+                sizes: '192x192',
+                type: 'image/png',
+              },
+              {
+                src: 'pwa-512x512.png',
+                sizes: '512x512',
+                type: 'image/png',
+              },
+            ],
+          },
+        }),
+      ],
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
